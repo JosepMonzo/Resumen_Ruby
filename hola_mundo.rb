004e239cef5912d3a19a80a48ae5d3398e07f746
josep <josepmetacromo@gmail.com>
2018-04-10 14:11:59 +0200

    caracteres especiales

--- hola_mundo.rb
+++ hola_mundo.rb
@@ -1,3 +1,30 @@
-print "Ingrese un nombre: "
-nombre = gets
+=begin
+Todo esto será comentado
+aunque hayan diferentes líneas hasta que
+se escriba el cierre end
+=end
+
+# Para comentar una sola linea
+
+# Una variable y con puts la escribimos por pantalla(dentro de String se le pone #{})
+
+nombre = 'Josep'
 puts "hola #{nombre}"
+
+
+
+#{nombre}
+#Escribir cadenas en diferentes lineas de diferentes maneras
+
+puts " hola
+mundo
+cruel"
+
+puts <<-CADENA
+Segundo ejemplo de hola
+mundo 
+cruel
+en diferentes lineas hasta no cerrar la cadena, usando palabras
+iguales en mayuscula tanto para abrir como para cerrar.
+CADENA
+
